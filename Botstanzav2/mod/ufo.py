699a2b6959c0d599960173686bdc7a3dcfda945c
TinnyTins <tepidteabag@gmail.com>
2019-05-09 22:31:55 +0100

    added random command for fun

--- Botstanzav2/mod/ufo.py
+++ Botstanzav2/mod/ufo.py
@@ -11,9 +11,17 @@ class ufo(object):
             return_message = ""
             city_search = message.content.split(" ",1)[1]
             if len(city_search) > 3:
-               cur.execute("select city from ufo where city like '%{0}%'".format(city_search))
+               cur.execute("select distinct city from ufo where city like '%{0}%'".format(city_search))
+               rows = cur.fetchall()
+               for row in rows:
+                   print(row)
             else:
                 return_message = "not enough characters in search"
+        if message.content.startswith("randomufo"):
+               cur.execute("select text from ufo ORDER BY RANDOM() LIMIT 1;")
+               rows = cur.fetchall()
+               for row in rows:
+                   await message.channel.send(row[0])
 
 class sighting(object):
     summary = ""
